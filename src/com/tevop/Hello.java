c6018ca40b751c60601f919444d53009e387a32d
tevop <tevop@163.com>
2019-02-04 15:25:21 +0800

    use annotation

--- src/com/tevop/Hello.java
+++ src/com/tevop/Hello.java
@@ -1,5 +1,8 @@
 package com.tevop;
 
+import org.springframework.stereotype.Component;
+
+@Component("hello")
 public class Hello {
 	public void hi() {
 		System.out.println("hello");
--- src/com/tevop/MyProcessor.java
+++ src/com/tevop/MyProcessor.java
@@ -2,18 +2,19 @@ package com.tevop;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-
+import org.springframework.stereotype.Component;
+@Component("processor")
 public class MyProcessor implements BeanPostProcessor{
 
 	@Override
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("before");
+		System.out.println("before: " + beanName);
 		return bean;
 	}
 
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("after");
+		System.out.println("after: " + beanName);
 		return bean;
 	}
 
